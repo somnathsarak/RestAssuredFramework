679fb7c97ccb60cf5b6c2e577b044bd26ce5b730
Somnath Sarak <spsarak123@gmail.com>
2026-01-22 16:44:44 +0530

    Add BaseTest - Base Selenium test class with setUp/tearDown

---
+++ src/main/java/base/BaseTest.java
@@ -0,0 +1,25 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        // For CI, point this to the Chrome/driver path or use WebDriverManager if allowed
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
